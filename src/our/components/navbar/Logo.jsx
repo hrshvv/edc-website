85c2b88804bc0bfb8b5a8095fa4e1250a028ecbc
Harsh <harsh.ver727@gmail.com>
2025-09-05 23:38:11 +0530

    reduced logo size

--- src/our/components/navbar/Logo.jsx
+++ src/our/components/navbar/Logo.jsx
@@ -9,7 +9,7 @@ const Logo = () => {
       {theme === "light" ? (
         // Light theme logo - you can replace this with your actual light logo
         <img
-          src="https://res.cloudinary.com/dh8cqlngr/image/upload/c_auto,f_auto,q_auto,g_center/v1756491588/EDC_New_Logo_light_qq2mqg.png"
+          src="https://res.cloudinary.com/dh8cqlngr/image/upload/v1757095434/EDC_New_Logo_light_h2jczl.png"
           alt="Logo"
           width={100}
           height={50}
@@ -17,7 +17,7 @@ const Logo = () => {
       ) : (
         // Dark theme logo - you can replace this with your actual dark logo
         <img
-          src="https://res.cloudinary.com/dh8cqlngr/image/upload/c_auto,f_auto,q_auto,g_center/v1756491576/EDC_New_Logo_darkbg_so3njq.png"
+          src="https://res.cloudinary.com/dh8cqlngr/image/upload/v1757095428/EDC_New_Logo_darkbg_b8ptlv.png"
           alt="Logo"
           width={100}
           height={50}
